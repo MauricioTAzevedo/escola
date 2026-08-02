8f18de5f5e1f679c83118bfab64fbbd8c377d635
Maurício Thales <164433651+MauricioTAzevedo@users.noreply.github.com>
2026-08-01 21:33:41 -0300

    fix(ci): add postgresql service container and prisma schema push to github actions

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/escola';
+}
+
 export const prisma = new PrismaClient();
